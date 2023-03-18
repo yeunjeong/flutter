e0fe7ee0749a04f75cb3147637ae87894973df4e
yeunjeong <araruu2536@gmail.com>
2023-03-18 21:21:18 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,122 @@
 import 'package:flutter/material.dart';
 
-/*
-runApp(메인 페이지): 앱 구동 함수
- */
 void main() {
   runApp(const MyApp());
 }
 
-/*
-"stless + Tab": 메인 페이지 생성
-class 이름 지정
-return 값 지정
- */
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
 
-    // 코딩 하는 공간
     return MaterialApp(
-      /*
-      기본 위젯 4가지
-      1. Text('안녕')
-      2. Icon(Icons.star): 아이콘의 이름은 flutter 홈페이지에서 찾을 수 있다.
-      3. Image.asset('경로'): 이미지 폴더를 pubspec.yaml 파일에 등록해줘야 한다.
-      4. Container(), SizedBox(): 파라미터로 스타일을 설정할 수 있다.
-       */
-      /*
-      Container 추가 설명
-      width와 height 숫자 단위는 LP로, 1LP = 약 1.2cm이다.
-      -> 하지만 화면을 꽉 채운다..
-      부모가 없기 때문!(시작점이 없음)
-      
-      부모 위젯 예시: Center()
-      -> 레이아웃에서 더 자세히 다룰 예정
-       */
-      home: Center(
-        child: Container( width: 50, height: 50, color: Colors.blue ),
-      )
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: TextButton(
+            child: Row(
+              children: [
+                Text("제목 ", style: TextStyle(color: Colors.black, fontSize: 20)),
+                Text("▼", style: TextStyle(color: Colors.black, fontSize: 10)),
+              ],
+            ),
+            onPressed: (){},
+          ),
+          actions: [
+            IconButton(
+              padding: EdgeInsets.zero,
+              constraints: BoxConstraints(),
+              onPressed: () {},
+              icon: const Icon(Icons.search, color: Colors.black),
+            ),
+            SizedBox( width: 20, height: 20,),
+            IconButton(
+              padding: EdgeInsets.zero,
+              constraints: BoxConstraints(),
+              onPressed: () {},
+              icon: const Icon(Icons.menu, color: Colors.black),
+            ),
+            SizedBox( width: 20, height: 20,),
+            IconButton(
+              padding: EdgeInsets.zero,
+              constraints: BoxConstraints(),
+              onPressed: () {},
+              icon: const Icon(Icons.notifications, color: Colors.black),
+            ),
+            SizedBox( width: 10, height: 20,),
+          ],
+        ),
+        body: Container(
+          margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                  width: 100, height: 100,
+                  margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Expanded(
+                      child: Image.asset('assets/images/lion.jpg')
+                  ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 100,
+                  margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                              width: double.infinity, height: 50,
+                              child: Text("datadnsdksssssssssssssssssssssssnsdadaslk", style: TextStyle(fontSize: 20))
+                          ),
+                          SizedBox(
+                              width: double.infinity, height: 15,
+                              child: Text("data", style: TextStyle(fontSize: 12))
+                          ),
+                          SizedBox(
+                              width: double.infinity, height: 15,
+                              child: Text("data", style: TextStyle(fontSize: 12))
+                          ),
+                          SizedBox(
+                            width: double.infinity, height: 5,
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width:15, height: 15,
+                            child: IconButton(
+                              padding: EdgeInsets.zero,
+                              constraints: BoxConstraints(),
+                              onPressed: () {},
+                              icon: const Icon(Icons.favorite_border),
+                              iconSize: 15,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )],
+          ),
+        ),
+      ),
     );
   }
 }
